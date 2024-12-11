b16e72446649144e4e7ca982ee6889f51eb89bce
Soysakiaytuno <152195270+Soysakiaytuno@users.noreply.github.com>
2024-12-11 12:26:41 -0400

    Interfaz actualizada
    xd

--- proyecto final progra 1/main.py
+++ proyecto final progra 1/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-
+def hola():
+    print("Hola xd")
 #raiz principal
 main = Tk()
 main.title("Base de datos god")
@@ -9,17 +10,34 @@ main.config(bg="#7BBECB")
 
 #fondo rojo
 elframe = Frame()
-elframe.pack(fill="both", expand="false")
+elframe.pack(fill="both", expand="true")
 elframe.config(bg="red")
 elframe.config(width="650", height="650")
 
 #imagen
 xd = PhotoImage(file="padoru.png")
 label = Label(image=xd)
-label.place(x="100",y="100")
+label.place(x="300",y="100")
 label.config(cursor="hand2")
 
 #Ingresar datos
-i = Entry(elframe)
-i.place(x=800,y=200)
+titulo = Label(elframe, text="BASE DE DATOS", font=("Comic Sans SN", 10))
+titulo.grid(row="0",column="1", padx=10, pady=10)
+driver = Entry(elframe)
+driver.grid(row="1",column="1", padx=10, pady=10)
+nombre_d = Label(elframe, text="Driver", font=("Comic Sans SN", 10))
+nombre_d.grid(row="1",column="0", padx=10, pady=10)
+server = Entry(elframe)
+server.grid(row="2",column="1", padx=10, pady=10)
+nombre_s = Label(elframe, text="Server", font=("Comic Sans SN", 10))
+nombre_s.grid(row="2",column="0", padx=10, pady=10)
+database = Entry(elframe)
+database.grid(row="3",column="1", padx=10, pady=10)
+nombre_db = Label(elframe, text="Data base", font=("Comic Sans SN", 10))
+nombre_db.grid(row="3",column="0", padx=10, pady=10)
+
+#boton
+boton = Button(main, text="Confirmar", command=hola)
+boton.pack()
+#main
 main.mainloop()
